b6a4fde697603595ed1a88e3df1ea2c22d1f1bd5
Rushaine McBean <codekid24@gmail.com>
2013-10-13 15:44:40 -0400

    Adds in the Aspell gem; Adds a route to validate spelling

--- app.rb
+++ app.rb
@@ -2,6 +2,8 @@ require 'rubygems'
 require 'compass' #must be loaded before sinatra
 require 'sass'
 require 'sinatra'
+require 'ffi/aspell'
+require 'json'
 require 'haml'    #must be loaded after sinatra
 
 # sets sinatra's variables
@@ -9,6 +11,9 @@ set :app_file, __FILE__
 set :root, File.dirname(__FILE__)
 set :views, "views"
 set :public_dir, 'static'
+
+#Define Aspell
+speller = FFI::Aspell::Speller.new('en_US')
 
 configure do
   set :haml, {:format => :html5, :escape_html => true}
@@ -23,4 +28,16 @@ end
 
 get '/' do
   haml :index
+end
+
+get '/spell/:word/correctly' do
+  word = params[:word]
+  return status 404 unless word
+
+  if speller.correct?(word)
+    return status 200
+  end
+
+  suggestions = speller.suggestions(word)
+  return suggestions.to_json
 end
